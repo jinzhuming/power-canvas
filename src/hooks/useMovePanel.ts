6d2f78c0d0ba8fc9cc4d0c4530553be119b5a383
jin <jinzhuming0308@gmail.com>
2022-07-08 22:54:33 +0800

    新增多选状态下的拖拽逻辑

--- src/hooks/useMovePanel.ts
+++ src/hooks/useMovePanel.ts
@@ -1,16 +1,25 @@
 /*
  * must be called in onMounted
  * */
-import { onUnmounted, toRefs } from 'vue';
-import { usePanelsStore } from '../stores/panels';
+import { computed, onUnmounted, toRefs } from 'vue';
+import { usePanelsStore, useSelectedPanelsStore } from '../stores/panels';
 import { useCanvasZoomStore } from '../stores/dashboard';
 
 export const useMovePanel = (panelID: string, panelDom: HTMLDivElement) => {
   const panelsStore = usePanelsStore();
   const canvasZoomStore = useCanvasZoomStore();
-
+  const selectedPanelsStore = useSelectedPanelsStore();
   const { panels } = toRefs(panelsStore);
   const { zoom } = toRefs(canvasZoomStore);
+  const { panels: selectedPanels } = toRefs(selectedPanelsStore);
+  const selectedPanelIDs = computed(() =>
+    selectedPanels.value.map((panel) => panel.id)
+  );
+
+  // 判断是不是多选状态
+  const isInMultipleSelected = computed(() =>
+    selectedPanelIDs.value.find((id) => id === panelID)
+  );
 
   // 这个是用来判断是否是点击导致的误操作
   const initMousePosition = {
@@ -43,15 +52,20 @@ export const useMovePanel = (panelID: string, panelDom: HTMLDivElement) => {
     prevMousePosition.x = currentX;
     prevMousePosition.y = currentY;
 
+    // 这里处理一个多选状态下的拖拽逻辑
     panelsStore.set(
       panels.value.map((panel) => {
-        if (panel.id === panelID) {
+        if (
+          panel.id === panelID ||
+          (isInMultipleSelected && selectedPanelIDs.value.includes(panel.id))
+        ) {
           return {
             ...panel,
             x: panel.x + movedX,
             y: panel.y + movedY
           };
         }
+
         return panel;
       })
     );
