12e473d1b119a638a1f3b584a6ee583af09ee7cd
jin <jinzhuming0308@gmail.com>
2022-07-14 16:47:07 +0800

    修改 vite 打包的 baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
